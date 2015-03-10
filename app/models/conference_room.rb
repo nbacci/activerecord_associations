1898a4f8ddaea370eda09023052f51c4f13ab892
nbacci <nmbachvarova@ucdavis.edu>
2015-03-10 14:47:38 -0700

    user can see the meetings with their times for a conference room

--- app/models/conference_room.rb
+++ app/models/conference_room.rb
@@ -1,2 +1,6 @@
 class ConferenceRoom < ActiveRecord::Base
+  has_many :reservations
+  has_many :meetings, through: :reservations
+
+  # Conference Room reservations Can see the meetings with their times for a conference room
 end
